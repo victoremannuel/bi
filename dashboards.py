f8e867c105e8c4887f03621adc6f19c4f44cea37
victor <vic.emannuel@gmail.com>
2023-11-16 21:59:29 -0300

    .

--- dashboards.py
+++ dashboards.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import subprocess
+subprocess.run(["pip", "install", "plotly"])
 
 st.set_page_config(layout="wide") #comando para usar a pagina toda
 
